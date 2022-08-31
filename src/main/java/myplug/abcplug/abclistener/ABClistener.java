0f07930c879a231a167c6667af76928d69a57e3e
WhatThe SillyName <bochkova820@gmail.com>
2022-08-31 19:39:08 +0300

    ~

--- src/main/java/myplug/abcplug/abclistener/ABClistener.java
+++ src/main/java/myplug/abcplug/abclistener/ABClistener.java
@@ -11,21 +11,24 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class ABClistener implements Listener {
 @EventHandler
-    public void onChat(AsyncChatEvent event) {
+    public void onChat(AsyncChatEvent event) {/*
     event.renderer((source, sourceDisplayName, message, viewer) ->
             Component.text()
                     .append(sourceDisplayName.color(TextColor.fromHexString("#00AA00")))
                     .append(Component.text(">>>").color(TextColor.fromHexString("#008800")))
                     .append(message.color(TextColor.fromHexString("#AAAAAA")))
                     .build());
+                    */
     }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
+    /*
         try {
             event.getPlayer().sendMessage(Component.text(ABCplug.config.getString("join")));
         } catch (Exception e) {
             e.printStackTrace();
         }
+     */
     }
 }
