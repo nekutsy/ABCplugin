e77b5d5f6ce272db48f3f11ef59e72f8240812ff
WhatThe SillyName <bochkova820@gmail.com>
2022-08-31 15:34:52 +0300

    ~

--- src/main/java/myplug/abcplug/abctouch/ABCtouch.java
+++ src/main/java/myplug/abcplug/abctouch/ABCtouch.java
@@ -3,6 +3,7 @@ package myplug.abcplug.abctouch;
 import com.google.common.eventbus.Subscribe;
 import myplug.abcplug.ABCplug;
 import net.kyori.adventure.text.Component;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.Event;
@@ -17,19 +18,24 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.event.entity.EntityInteractEvent;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityInteractEvent;
+import sun.security.provider.ConfigFile;
+import javax.security.auth.login.Configuration;
+import java.nio.channels.FileChannel;
 
 public class ABCtouch implements Listener {
     @EventHandler
     public void BlockBreakEvent(PlayerInteractEvent event) {
         Block b = event.getClickedBlock();
         Player p = event.getPlayer();
+        FileConfiguration conf = ABCplug.config;
+        FileConfiguration userConf = ABCplug.users;
 
         p.chat(b.getType().toString());
         String name = "x" + b.getX() + "y" + b.getY() + "z" + b.getZ();
-        if (ABCplug.config.contains(name)) {
+        if (conf.contains(name) && !userConf.contains(conf.getString(name))) {
             p.sendMessage(Component.text("+9999999 MONEY"));
-            ABCplug.users.set(p.getName().toString() + name, true);
+            userConf.set(p.getName() + "." + conf.getString(name), name);
+
         }
-        ABCplug.users.set(p.getName().toString() + "." + name, true);
     }
 }
