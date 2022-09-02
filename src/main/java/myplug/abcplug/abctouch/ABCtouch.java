178170150a649b3ad2aef41d1bf87d62b6b7a015
WhatThe SillyName <bochkova820@gmail.com>
2022-09-02 19:39:48 +0300

    ~

--- src/main/java/myplug/abcplug/abctouch/ABCtouch.java
+++ src/main/java/myplug/abcplug/abctouch/ABCtouch.java
@@ -29,6 +29,32 @@ import org.bukkit.Bukkit;
 
 public class ABCtouch implements Listener {
     @EventHandler
+    public void BlockBreakEvent(PlayerInteractEvent event) {
+        try {
+            Block b = event.getClickedBlock();
+            Player p = event.getPlayer();
+            FileConfiguration conf = ABCplug.config;
+            FileConfiguration userConf = ABCplug.users;
+
+            //p.chat(b.getType().toString());
+            String name = "x" + b.getX() + "y" + b.getY() + "z" + b.getZ();
+            if (conf.contains(name)) {
+                if (!userConf.contains(p.getName())) {
+                    userConf.createSection(p.getName());
+                } else {
+                    if (conf.getString(name + ".name") != null && !userConf.contains(p.getName() + "." + conf.getString(name + ".name"))) {
+                        Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), conf.getString(name + ".command").replace("_player", p.getName()));
+                        userConf.set(p.getName() + "." + conf.getString(name + ".name"), name);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    /*
+
+    @EventHandler
     public void PlayerInteractEvent(@NotNull Player who, @NotNull Action action, @Nullable ItemStack item, @Nullable Block clickedBlock, @NotNull BlockFace clickedFace) {
         if (action.isLeftClick()) {
             try {
@@ -54,6 +80,7 @@ public class ABCtouch implements Listener {
             }
         }
     }
+     */
 }
 
 /*
